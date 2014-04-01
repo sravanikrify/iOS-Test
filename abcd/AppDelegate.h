1cf2330f4b7538425c32a5c9deb7d2d4f3d3d011
Sravani <krishnareddykovvuri@Sravanis-Mac-mini.local>
2014-04-01 14:21:13 +0530

    Initial Commit

---
+++ abcd/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  abcd
+//
+//  Created by Sravani on 01/04/14.
+//  Copyright (c) 2014 Krify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ abcd/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  abcd
+//
+//  Created by Sravani on 01/04/14.
+//  Copyright (c) 2014 Krify. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
